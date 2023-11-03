4115db5df489701f005583bc8b3f12f066350612
0x01010 <92220159+OFN01@users.noreply.github.com>
2023-11-03 17:46:39 +0300

    v1

--- app.py
+++ app.py
@@ -11,12 +11,10 @@ import ctypes
 import getpass
 import os
 import shutil
-import win32gui, win32con
+import win32gui
 import sys
 
-hiding = True
 hwnd = win32gui.GetForegroundWindow()
-win32gui.ShowWindow(hwnd , win32con.SW_HIDE)
 USER_NAME = getpass.getuser()
 
 def _add_to_startup(file_path=""):
@@ -53,139 +51,166 @@ def remove_from_startup():
 def install():
     _add_to_programs()
 
-def remove():
+def uninstall():
     remove_from_startup()
     if os.path.exists((r"C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" % USER_NAME) + '\\' + "AutoClicker.lnk"):
         os.remove((r"C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" % USER_NAME) + '\\' + "AutoClicker.lnk")
 
-for i in sys.argv[1:]:
-    if i == '-addstartup':
-        add_to_startup()
-    if i == '-rmstartup':
-        remove_from_startup()
-    if i == '-install':
-        install()
-    if i == '-remove':
-        install()
+def main():
+    import win32api, win32con
 
-user32 = ctypes.windll.user32
-import win32api, win32con
+    hiding = True
+    win32gui.ShowWindow(hwnd , win32con.SW_HIDE)
 
+    user32 = ctypes.windll.user32
 
-def lclick(down=True):
-    x, y = win32api.GetCursorPos()
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    def lclick(down=True):
+        x, y = win32api.GetCursorPos()
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
 
-def rclick():
-    x, y = win32api.GetCursorPos()
-    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
+    def rclick():
+        x, y = win32api.GetCursorPos()
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
 
-running = False
-pressing1 = False
-pressing2 = False
+    running = False
+    pressing1 = False
+    pressing2 = False
 
 
-def t():
-    XBUTTON1 = 0x0001
-    XBUTTON2 = 0x0002
+    def t():
+        XBUTTON1 = 0x0001
+        XBUTTON2 = 0x0002
 
-    wm = {
-        0x020B: "WM_XBUTTONDOWN",
-        0x020C: "WM_XBUTTONUP",
-        0x0201: "WM_LBUTTONDOWN",
-    }
+        wm = {
+            0x020B: "WM_XBUTTONDOWN",
+            0x020C: "WM_XBUTTONUP",
+            0x0201: "WM_LBUTTONDOWN",
+        }
 
-    def mouse_handler(msg, x, y, data, flags, time, hwnd, window_name):
-        name = wm.get(msg, None)
-        if name and running:
-            xb = data >> 16
-            global pressing1
-            global pressing2
-            if name == "WM_XBUTTONDOWN":
-                if xb == 1:
-                    pressing1 = True
-                elif xb == 2:
-                    pressing2 = True
+        def mouse_handler(msg, x, y, data, flags, time, hwnd, window_name):
+            name = wm.get(msg, None)
+            if name and running:
+                xb = data >> 16
+                global pressing1
+                global pressing2
+                if name == "WM_XBUTTONDOWN":
+                    if xb == 1:
+                        pressing1 = True
+                    elif xb == 2:
+                        pressing2 = True
+                    return False
+                if name == "WM_XBUTTONUP":
+                    if xb == 1:
+                        pressing1 = False
+                    elif xb == 2:
+                        pressing2 = False
+                    return False
+                # if name == "WM_LBUTTONDOWN":
+                #    user32.PostQuitMessage(0)
+            return True
+
+        def OnKeyboardEvent(event):
+            if event.KeyID == 123 and GetKeyState(HookConstants.VKeyToID('VK_RSHIFT')) == 128: # SHIFT F-12
+                global running
+                print('Stopped' if running else 'Running')
+                running = not running
                 return False
-            if name == "WM_XBUTTONUP":
-                if xb == 1:
-                    pressing1 = False
-                elif xb == 2:
-                    pressing2 = False
+            if event.KeyID == 122 and GetKeyState(HookConstants.VKeyToID('VK_RSHIFT')) == 128: # SHIFT F-11
+                global hiding
+                if hiding:
+                    win32gui.ShowWindow(hwnd , win32con.SW_SHOW)
+                else:
+                    win32gui.ShowWindow(hwnd , win32con.SW_HIDE)
+                hiding = not hiding
                 return False
-            # if name == "WM_LBUTTONDOWN":
-            #    user32.PostQuitMessage(0)
-        return True
-    
-    def OnKeyboardEvent(event):
-        if event.KeyID == 123 and GetKeyState(HookConstants.VKeyToID('VK_RSHIFT')) == 128: # SHIFT F-12
-            global running
-            print('Stopped' if running else 'Running')
-            running = not running
-            return False
-        if event.KeyID == 122 and GetKeyState(HookConstants.VKeyToID('VK_RSHIFT')) == 128: # SHIFT F-11
-            global hiding
-            if hiding:
-                win32gui.ShowWindow(hwnd , win32con.SW_SHOW)
-            else:
-                win32gui.ShowWindow(hwnd , win32con.SW_HIDE)
-            hiding = not hiding
-            return False
-        return True
+            return True
+
+        try:
+            hm = HookManager()
+            hm.KeyDown = OnKeyboardEvent
+            hm.HookKeyboard()
+            cpyHook.cSetHook(HookConstants.WH_MOUSE_LL, mouse_handler)
+            pythoncom.PumpMessages() # Loop
+        finally:
+            cpyHook.cUnhook(HookConstants.WH_MOUSE_LL)
 
     try:
-        hm = HookManager()
-        hm.KeyDown = OnKeyboardEvent
-        hm.HookKeyboard()
-        cpyHook.cSetHook(HookConstants.WH_MOUSE_LL, mouse_handler)
-        pythoncom.PumpMessages() # Loop
-    finally:
-        cpyHook.cUnhook(HookConstants.WH_MOUSE_LL)
+        with open(os.path.join(os.path.abspath(os.path.dirname(__name__), '.conf')), 'r') as f:
+            target_cps = int(f.read())  # Change this to set cps what you want
 
-try:
-    target_cps = 20  # Change this to set cps what you want
+        thethread = threading.Thread(target=t, daemon=True)
+        thethread.start()
 
-    thethread = threading.Thread(target=t, daemon=True)
-    thethread.start()
-
-    timer = time.time()
-    wait = 1 / target_cps
-    error = 0
-    while True:
-        if not running:
-            timer = time.time()
-            time.sleep(0.5)
-            continue
-        if  not (pressing1 or pressing2):
-            time.sleep(0.01)
-            continue
-        try:
-            x, y = win32api.GetCursorPos()
-        except:
-            running = False
-            print('Stopped (Due to error like you are writing passwords, etc.)')
-            continue
-        pl = pressing1
-        pr = pressing2
-        if pl:
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-        if pr:
-            win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
-        while time.time() - timer < ((1 / target_cps) - error)/2:
-            pass
-        now = time.time()
-        if pl:
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-        if pr:
-            win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
-        while now - timer < (1 / target_cps) - error:
+        timer = time.time()
+        wait = 1 / target_cps
+        error = 0
+        while True:
+            if not running:
+                timer = time.time()
+                time.sleep(0.5)
+                continue
+            if  not (pressing1 or pressing2):
+                time.sleep(0.01)
+                continue
+            try:
+                x, y = win32api.GetCursorPos()
+            except:
+                running = False
+                print('Stopped (Due to error like you are writing passwords, etc.)')
+                continue
+            pl = pressing1
+            pr = pressing2
+            if pl:
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+            if pr:
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
+            while time.time() - timer < ((1 / target_cps) - error)/2:
+                pass
             now = time.time()
+            if pl:
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+            if pr:
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
+            while now - timer < (1 / target_cps) - error:
+                now = time.time()
 
-        error += (now - timer) - (1 / target_cps)
-        timer = now
-except KeyboardInterrupt:
-    print('\nQuit')
-    user32.PostQuitMessage(0)
+            error += (now - timer) - (1 / target_cps)
+            timer = now
+    except KeyboardInterrupt:
+        print('\nQuit')
+        user32.PostQuitMessage(0)
+    quit()
+
+helptext = """AutoClicker by Ömer Faruk Nehir (https://github.com/OFN01)
+[ --help       , -h       ]: Show this message and exit
+[ --addstartup , -S       ]: Add program to startup as user
+[ --rmstartup  , -s       ]: Remove program from startup
+[ --install    , -i       ]: Install this program as user (Still needs this python file and contents)
+[ --uninstall  , -u       ]: Uninstall this program from system and remove from startup
+[ --cps={cps}  , -c={cps} ]: Set clicks per second of AutoClicker"""
+
+for arg in sys.argv[1:]:
+    if arg in ['--help', '-h', '/h', '/help']:
+        print(helptext)
+    elif arg in ['--addstartup', '-S']:
+        add_to_startup()
+    elif arg in ['--rmstartup', '-s']:
+        remove_from_startup()
+    elif arg in ['--install', '-i']:
+        install()
+    elif arg in ['--uninstall', '-u']:
+        uninstall()
+    elif arg.startswith('--cps=') and arg.removeprefix('--cps=').isnumeric():
+        with open(os.path.join(os.path.abspath(os.path.dirname(__name__), '.conf')), 'w') as f:
+            f.write(arg.removeprefix('--cps='))
+    elif arg.startswith('-c=') and arg.removeprefix('-c=').isnumeric():
+        with open(os.path.join(os.path.abspath(os.path.dirname(__name__), '.conf')), 'w') as f:
+            f.write(arg.removeprefix('-c='))
+    else:
+        print(f'Unknown option: {arg}\nUse -h for more information.')
+
+if len(sys.argv) == 1 or '--run' in sys.argv[1:]:
+    main()
